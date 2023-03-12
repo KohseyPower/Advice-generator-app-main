797d3cf92745db78805b9d4af39a9d510bda5e8c
Kohsey <kohsey.dufour@gmail.com>
2023-03-12 21:38:00 +0100

    truvc

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ window.onload = function() {
 async function message() {
     const res = await fetch('https://api.adviceslip.com/advice');
     const data = await res.json();
-
-    document.querySelector(".message").textContent = data.slip.advice;
+    document.querySelector(".id-message").textContent = "ADVICE # " + data.slip.id;
+    document.querySelector(".message").textContent = "\"" + data.slip.advice + "\"";
 
 }
